c301b44561d9f45e522cfe745edfa53ee90d4a78
Aditya_Misal <aditya.misal02@gmail.com>
2024-02-12 16:29:59 +0530

    small changes in navbar

--- Ecommerce/e-commerce/src/Components/NavBar.js
+++ Ecommerce/e-commerce/src/Components/NavBar.js
@@ -5,6 +5,13 @@ import Login from '../Icons/login.png';
 import Search from '../Icons/search.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 const NavBar = () => {
+
+  const handleKeyPress = (event) =>{
+    if(event.key ==="Enter"){
+      console.log(event.target.value);
+    }
+  }
+
   return (
     <div >
       <div >                            
@@ -12,13 +19,17 @@ const NavBar = () => {
         <span style={{ marginLeft: '10px', color: '#fff' , marginTop : '7px',font:'16px'}}>Logo</span>
           <span style={{ marginLeft: '20px', color: '#fff' , marginTop : '7px', fontSize:'20px'}}>Home</span>
           <span style={{ marginLeft: '20px', color: '#fff' , marginTop : '7px' ,fontSize:'20px'}}>Profile</span>
-          <span style={{ marginLeft: '120px'}}>
+          <span style={{ marginLeft: '100px'}}>
                 <input
                     type="text"
                     placeholder="Search"
+                    onKeyPress={handleKeyPress}
                     style={{ padding: '10px', borderRadius: '5px', border: '1px solid #ccc', width: '840px' }}/>
           </span>
-          <span style={{ marginLeft: '10px', marginRight: '2px', marginTop:'6px' }}><img src={Search} style={{width:'25px', height:'25px'}} alt="search"></img></span> 
+          {/* <span style={{ marginLeft: '10px', marginRight: '2px', marginTop:'6px' }}><img src={Search} style={{width:'25px', height:'25px'}} alt="search"></img></span>  */}
+          <button type="button" className="btn btn-success d-flex align-items-center justify-content-center" style={{width:'80px', height:'35px', marginTop:'10px'}}>
+                Search
+          </button>          
           <span style={{ marginLeft: '90px', marginRight: '2px', marginTop:'10px' }}><img src={Wallet} style={{width:'25px', height:'25px'}} alt="wallet"></img></span>
           <span style={{ marginLeft: '25px', marginRight: '20px', marginTop:'10px'  }}><img src={Cart} style={{width:'25px', height:'25px'}} alt="Cart"></img></span>
           <span style={{ marginLeft: '5px', marginRight: '20px', marginTop:'10px', color:'white' }}><img src={Login} style={{width:'25px', height:'25px'}} alt="Login"></img></span>
